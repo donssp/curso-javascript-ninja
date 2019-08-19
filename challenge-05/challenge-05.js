4738e81d2870dccd77c45b8600c0dbb4f3a424fa
unknown <mastertrash@gmail.com>
2019-08-19 16:50:39 -0300

    Challenge 5.0

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,19 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var arr = [0, '', 'dondon', undefined, 666, true];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+function rec_array(arr){
+	return arr;
+};
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+rec_array(arr)[1];
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,19 +22,26 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+function index_arr(arr, id){
+	return arr[id];
+};
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+var difArray = [1, 'string', undefined, true, -0, NaN];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
-
+index_arr(difArray, 0);	// 1
+index_arr(difArray, 1); // 'string'
+index_arr(difArray, 2); // undefined
+index_arr(difArray, 3); // true
+index_arr(difArray, 4); // -0
+index_arr(difArray, 5); // NaN
+ 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
 livro. Dentro dessa função, declare uma variável que recebe um objeto com as
@@ -47,7 +56,15 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+function book(bookName){
+	var allBooks = {
+		'book1':{qtdPagina:44, autor:'ze', editora:'indie'},
+		'book2':{qtdPagina:22, autor:'juca', editora:'zaza'},
+		'book3':{qtdPagina:3333, autor:'tiao', editora:'texas'}
+	}
+
+	return bookName ? allBooks[bookName] : allBooks;
+};
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
