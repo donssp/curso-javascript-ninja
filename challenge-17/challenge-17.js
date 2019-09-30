17e416bb2d629ef2b2f7755f73d8c20688e232bb
unknown <fatecitu@fiti02.acd.fatecitu.local>
2019-09-30 13:27:57 -0300

    challenge17.0

--- challenge-17/challenge-17.js
+++ challenge-17/challenge-17.js
@@ -1,8 +1,10 @@
+(function(){
 /*
 1. Envolva todo o conteúdo desse desafio em uma IIFE.
 2. Adicione a diretiva 'use strict';
 3. Crie um arquivo index.html e adicione esse script à ele.
 */
+'use script';
 
 /*
 Em todos os exercícios desse desafio, nós vamos utilizar expressões
@@ -10,7 +12,7 @@ regulares! Para isso, iremos usar o texto abaixo. Coloque-o em uma
 variável chamada `text`:
 "Manuel Marques de Sousa, Conde de Porto Alegre (Rio Grande, 13 de junho de 1804 – Rio de Janeiro, 18 de julho de 1875), apelidado de "O Centauro de Luvas", foi um militar, político, abolicionista e monarquista brasileiro."
 */
-// ?
+text = 'Manuel Marques de Sousa, Conde de Porto Alegre (Rio Grande, 13 de junho de 1804 – Rio de Janeiro, 18 de julho de 1875), apelidado de "O Centauro de Luvas", foi um militar, político, abolicionista e monarquista brasileiro.';
 
 /*
 Vamos começar com umas brincadeiras fáceis :D
@@ -18,7 +20,7 @@ Troque o nome "Manuel Marques de Sousa" pelo seu nome, e mostre o resultado
 no console:
 */
 console.log( 'Adicionando seu nome no texto:' );
-// ?
+console.log(text.replace(/Manuel Marques de Sousa/, 'Eu mesmo da silva'));
 
 /*
 Agora, substitua a palavra "brasileiro" por sua cidade natal e mostre no
@@ -26,35 +28,34 @@ console.
 Ex: Se você for da São Paulo, substitua por "paulista".
 */
 console.log( '\nTrocando naturalidade:' );
-// ?
+console.log(text.replace(/brasileiro/g, 'Pindamonhangabense'));
 
 /*
 Substitua todos os números por um traço `-`. Cada caractere de número deve
 ser um traço. Mostre o resultado no console:
 */
 console.log( '\nTrocando números por -:' );
-// ?
+console.log(text.replace(/\d/g,'-'));
 
 /*
 Substitua todas as letras (somente letras) de "D" maiúsculo até "h"
 minúsculo por "0" (número zero). Mostre o resultado no console:
 */
 console.log( '\nTrocando de "D" a "h" por "0":' );
-// ?
+console.log(text.replace(/[D-Za-h]/g, '0'));
 
 /*
 Substitua todos os "A" (maiúsculos ou minúsculos) por "4".
 Mostre o resultado no console:
 */
 console.log( '\nTrocando "A" e "a" por "4":' );
-// ?
+console.log(text.replace(/[Aa]/g, '4'));
 
 /*
 Substitua a frase "O Centauro de Luvas", deixando-a em caixa alta, usando
 o método `toUpperCase()`. Mostre o resultado no console:
 */
-console.log( '\n"O Centauro de Luvas" em caixa alta:' );
-// ?
+
 
 /*
 Agora iremos substituir as datas no formato "13 de junho de 1804" para
@@ -91,3 +92,4 @@ console o resultado.
 */
 console.log( '\nReplace de datas:' );
 // ?
+})();
